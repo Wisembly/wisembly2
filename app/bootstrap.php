2313604f724f0690bd9b231c3fe54ca44d6470d5
Guillaume Potier <cobalt2760@gmail.com>
2013-08-04 18:00:39 +0200

    - updated silexCMS to 0.2.2. Refacto config file

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,57 +1,33 @@
 <?php
 
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\HttpFoundation\Request;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// fetch global config
-$config = require_once __DIR__ . '/config/config.php';
-$locale = isset($locale) ? $locale : $config['default.locale'];
+use Symfony\Component\Yaml\Yaml;
+use SilexCMS\Repository\GenericRepository;
 
-// fetch databases config
-$dbOptions = require_once __DIR__ . '/config/database.php';
-$localizedDbOptions = $dbOptions[$locale];
-$localizedDbOptions['charset'] = 'UTF8';
+$config = Yaml::parse(__DIR__.'/config/config.yml');
+$locale = isset($locale) ? $locale : $config['global']['locale'];
 
 // instanciate our SilexCMS
-$app = new SilexCMS\Application(array(
-    'locale_fallback'       => $config['default.locale'],
-    'locale'                => $locale,
-    'twig.path'             => __DIR__ . '/../src/Application/Resources/views',
-    'twig.options'          => array(
-        'debug' => $config['debug'],
-        'cache' => __DIR__ . '/cache',
-    ),
-    'db.options'            => $localizedDbOptions,
-    'silexcms.cache'        => array(
-        'active' => !$config['debug'],
-        'type'   => isset($config['cache']) ? $config['cache'] : 'array',
-    ),
-    'silexcms.security'     => require_once __DIR__ . '/config/users.php',
+$app = new SilexCMS\Application(array_merge(
+    $config,
+    array(
+        'twig.path'         => __DIR__ . '/../src/Application/Resources/views',
+        'twig.options'      => array(
+            'debug'         => $config['global']['debug'],
+            'cache'         => __DIR__ . '/cache',
+        ),
+        'db.options'        => array_merge($config['databases']['default'], $config['databases'][$locale]),
+    )
 ));
-$app['silexcms_locale'] = $locale;
-$app['silexcms_full_db_options'] = $dbOptions;
 
-// phone numbers
-$app['phone.numbers'] = array(
-    'en'    =>  '+442036089022',
-    'fr'    =>  '0142615947',
-    'de'    =>  '+498007234918',
-    'es'    =>  '+34931807007',
-    'ch'    =>  '+41435083520',
-    'be'    =>  '+3225881720',
-);
-
+// country & phone numbers
 $app['country'] = isset($country) ? $country : 'fr';
-$app['phone_number'] = $app['phone.numbers'][$app['country']];
+$app['phone_number'] = $config['phone.numbers'][$app['country']];
 
-// ** load messages **
-$messages = $app['db']->executeQuery('SELECT * FROM messages')->fetchAll();
-
-if (false === $messages) {
-    $messages = array();
-}
+// ** load DB messages **
+$repo = new GenericRepository($app['db'], 'messages');
+$messages = $repo->findAll();
 
 foreach ($messages as $key => $message) {
     $messages[$locale][$message['message_key']] = $message['message_value'];
@@ -66,16 +42,18 @@ $app['translator.domains'] = array(
 $app['twig']->addExtension(new SilexCMS\Twig\Extension\ForeignKeyExtension($app));
 $app['twig']->addExtension(new Application\Twig\Extension\AssetsExtension());
 
-if ($config['debug']) {
+if ($config['global']['debug']) {
     $app['debug'] = true;
     $app['twig']->addExtension(new Twig_Extensions_Extension_Debug());
     $app['twig']->enableDebug();
 }
 
+// swiftmailer
 $app->register(new Silex\Provider\SwiftmailerServiceProvider(), array(
     'swiftmailer.options'   => require_once __DIR__ . '/config/mailer.php',
 ));
 
+unset($config);
 require_once __DIR__ . '/startup.php';
 
 return $app;
