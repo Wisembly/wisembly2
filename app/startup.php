1d06919ba9d33a5431d77b2930666033d4c90a95
Tristan Daeschner <tristan@wisembly.com>
2013-08-06 10:37:20 +0200

    - added templates

--- app/startup.php
+++ app/startup.php
@@ -3,10 +3,14 @@
 use Silex\Application;
 
 use SilexCMS\Set\DataSet,
-    SilexCMS\Page\StaticPage;
+    SilexCMS\Page\StaticPage,
+    SilexCMS\Page\DynamicPage;
 
 // DataSets
 $app->register(new DataSet('main', 'main'));
 
 // Pages
 $app->register(new StaticPage('index', '/', 'pages/index.html.twig'));
+$app->register(new StaticPage('training', '/events', 'pages/events.html.twig'));
+$app->register(new StaticPage('training', '/meetings', 'pages/meetings.html.twig'));
+$app->register(new StaticPage('training', '/trainings-classes', 'pages/trainings.html.twig'));
