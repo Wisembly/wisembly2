1bbbc6ee4e14ed845c95d606ea7b6731e3e4d48a
Tristan Daeschner <tristan@wisembly.com>
2013-08-20 11:34:43 +0200

    - added plans page

--- app/startup.php
+++ app/startup.php
@@ -14,3 +14,4 @@ $app->register(new StaticPage('trainings', '/for-classes-and-trainings', 'pages/
 $app->register(new StaticPage('meetings', '/for-meetings-and-confcalls', 'pages/meetings.html.twig'));
 $app->register(new StaticPage('events', '/for-events-and-seminaries', 'pages/events.html.twig'));
 $app->register(new StaticPage('case-studies', '/case-studies', 'pages/casestudies.html.twig'));
+$app->register(new StaticPage('plans', '/plans', 'pages/plans.html.twig'));
