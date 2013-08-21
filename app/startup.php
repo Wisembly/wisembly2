8e6d4088b0027cb05bf870792123a657a58a594a
Tristan Daeschner <tristan@wisembly.com>
2013-08-21 14:54:03 +0200

    - added cases pages

--- app/startup.php
+++ app/startup.php
@@ -14,6 +14,10 @@ $app->register(new StaticPage('trainings', '/for-classes-and-trainings', 'pages/
 $app->register(new StaticPage('meetings', '/for-meetings-and-confcalls', 'pages/meetings.html.twig'));
 $app->register(new StaticPage('events', '/for-events-and-seminaries', 'pages/events.html.twig'));
 $app->register(new StaticPage('case-studies', '/case-studies', 'pages/casestudies.html.twig'));
+$app->register(new StaticPage('case-la-poste', '/case-studies/la-poste', 'pages/case-la-poste.html.twig'));
+$app->register(new StaticPage('case-sncf', '/case-studies/sncf', 'pages/case-sncf.html.twig'));
+$app->register(new StaticPage('case-auchan', '/case-studies/auchan', 'pages/case-auchan.html.twig'));
+$app->register(new StaticPage('case-hec', '/case-studies/hec', 'pages/case-hec.html.twig'));
 $app->register(new StaticPage('plans', '/plans', 'pages/plans.html.twig'));
 $app->register(new StaticPage('about', '/about', 'pages/about.html.twig'));
 $app->register(new StaticPage('resources', '/about/resources', 'pages/resources.html.twig'));
