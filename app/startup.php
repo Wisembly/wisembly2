fe11e2398bb33ddb83012711b2bb01ea5b910c7c
Guillaume Potier <cobalt2760@gmail.com>
2013-09-02 12:07:37 +0200

    - updated studie cases pages and localizations

--- app/startup.php
+++ app/startup.php
@@ -3,7 +3,9 @@
 use Silex\Application;
 
 use SilexCMS\Set\LocalizationSet,
-    SilexCMS\Page\StaticPage;
+    SilexCMS\Set\DataSet,
+    SilexCMS\Page\StaticPage,
+    SilexCMS\Page\DynamicPage;
 
 /**
  * LocalizationsSets
@@ -13,26 +15,30 @@ $app->register(new LocalizationSet('features_meeting', 'features_meeting', 'key'
 $app->register(new LocalizationSet('features_event', 'features_event', 'key'));
 $app->register(new LocalizationSet('homepage', 'homepage', 'key'));
 
+/**
+ * DataSets
+ */
+$app->register(new DataSet('cases', 'cases'));
+
 // Pages
 $app->register(new StaticPage('index', '/', 'pages/index.html.twig'));
 
+// features
 $app->register(new StaticPage('classes-trainings', '/for-classes-and-trainings', 'pages/classes-trainings.html.twig'));
 $app->register(new StaticPage('meetings-confcalls', '/for-meetings-and-conf-calls', 'pages/meetings-confcalls.html.twig'));
 $app->register(new StaticPage('events-seminaries', '/for-events-and-seminaries', 'pages/events-seminaries.html.twig'));
 
+// case studies
 $app->register(new StaticPage('case-studies', '/case-studies', 'pages/casestudies.html.twig'));
+$app->register(new DynamicPage('case-detail', '/case-studies/{slug}', 'pages/case-detail.html.twig', 'cases'));
 
-$app->register(new StaticPage('case-la-poste', '/case-studies/la-poste', 'pages/case-la-poste.html.twig'));
-$app->register(new StaticPage('case-sncf', '/case-studies/sncf', 'pages/case-sncf.html.twig'));
-$app->register(new StaticPage('case-auchan', '/case-studies/auchan', 'pages/case-auchan.html.twig'));
-$app->register(new StaticPage('case-hec', '/case-studies/hec', 'pages/case-hec.html.twig'));
-
+// create your freemium now
 $app->register(new StaticPage('plans', '/plans', 'pages/plans.html.twig'));
 
+// about pages
 $app->register(new StaticPage('about', '/about', 'pages/about.html.twig'));
 $app->register(new StaticPage('about-team', '/about-team', 'pages/about-team.html.twig'));
 $app->register(new StaticPage('about-press', '/about-press', 'pages/about-press.html.twig'));
-
 $app->register(new StaticPage('resources', '/about/resources', 'pages/resources.html.twig'));
 $app->register(new StaticPage('jobs', '/jobs', 'pages/jobs.html.twig'));
 $app->register(new StaticPage('contact', '/contact', 'pages/contact.html.twig'));
