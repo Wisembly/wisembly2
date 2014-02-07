8aebf1202256288ea2e0980649664c800af73461
Mathieu Ghaleb <mathieu@wisembly.com>
2014-02-07 16:45:58 +0100

    - more mixpanel corpo:page events

--- web/static/js/mixpanel.js
+++ web/static/js/mixpanel.js
@@ -1,19 +1,41 @@
 $(document).ready(function () {
-// page plans visit
-if ($('body.plans').length) {
-  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'plans' });
+
+
+if ($(document.body).hasClass('frontpage')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'home' });
   mixpanel.people.set( persistCampaign );
 }
+
+if ($(document.body).hasClass('plans')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'plans' });
+}
+
+if ($(document.body).hasClass('meetings-confcalls')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'confcall' });
+}
+
+if ($(document.body).hasClass('events-seminaries')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'event' });
+}
+
+if ($(document.body).hasClass('classes-trainings')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'training' });
+}
+
+if ($(document.body).hasClass('case-studies')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'case_studies' });
+}
+
+if ($(document.body).hasClass('contact')) {
+  mixpanel.track( 'corpo:page', { app: 'corpo', page: 'contact' });
+}
+
 
 // clicks on create wiz button
 mixpanel.track_links('#plan-create-meeting', 'corpo:wiz:create', { app: 'corpo', context: 'freemium', product: 'confcall' });
 mixpanel.track_links('#plan-create-training', 'corpo:wiz:create', { app: 'corpo', context: 'freemium', product: 'training' });
 mixpanel.track_links('#plan-create-event', 'corpo:wiz:create', { app: 'corpo', context: 'freemium', product: 'event' });
 
-// clicks on home page view product features
-mixpanel.track_links('#hp-feature-meeting', 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'confcall' });
-mixpanel.track_links('#hp-feature-training', 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'training' });
-mixpanel.track_links('#hp-feature-event', 'corpo:page', { app: 'corpo', page: 'wiz_feature', product: 'event' });
 
 var persistCampaign = {
     adwordsCampaign: getAdwordsCampaign()
